323c04cfcec2d4c2ce9c90fd0f312d450393d7aa
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2024-09-21 07:45:18 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const socketio = require("socket.io");
+const { Socket } = require("dgram");
+const io = socketio(server);
+
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",function(socket){
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{id: socket.id, ...data});
+    });
+    socket.on("disconnect",function(){
+        io.emit("user-disconnected",socket.id);
+    });
+})
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+server.listen(3000);
