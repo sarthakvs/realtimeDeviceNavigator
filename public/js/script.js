20b154e19ac6e9dbb9dc0b84e39184a6898b1856
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2024-09-21 08:52:14 +0530

    removed unnecessary code

--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,7 @@ if(navigator.geolocation){
 }
 const map = L.map("map").setView([0,0],16);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"by sarthakvs"
+    attribution:"by github.com/sarthakvs"
 }).addTo(map);
 
 const markers = {};
